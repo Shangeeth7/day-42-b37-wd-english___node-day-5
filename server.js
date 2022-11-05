b814b0c5435a0d6cad7cf4a2f5aacf1e85cc0eca
SHANGEETH <shanshangeeth@gmail.com>
2022-11-05 10:30:13 +0530

    s

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ app.use(express.json());
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
 const mongodbConnection = require("./config/mongodbConnection");
-const port = 6572;
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.get("/", (req, res) => res.send("Hello World!"));
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
+}
+const port = 6572;
 app.listen(port, () => console.log(`Node JS Server Running On Port ${port}!`));
